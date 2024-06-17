e89ef11607b3ff3dc7b7ef4a9b8e8407c9c98d0b
xiaoling-yang <yxl1939748084@163.com>
2024-06-17 18:14:56 +0800

    2024.6.17

---
+++ Car_Terminal_6_17/Core/Inc/MyCallbackFunc.h
@@ -0,0 +1,9 @@
+#ifndef  __MYCALLBACKFUNC_H
+#define  __MYCALLBACKFUNC_H
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "zw0623.h"
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);//外部中断回调处理函数
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);//定时器中断回调函数
+#endif
