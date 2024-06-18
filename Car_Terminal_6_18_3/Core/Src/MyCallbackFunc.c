ebde595194f0aad2c0b3e5a94b9cdeca1bb97ebe
xiaoling-yang <yxl1939748084@163.com>
2024-06-18 15:37:16 +0800

    2024.6.18.3优化指纹容量和录入ID的大小

---
+++ Car_Terminal_6_18_3/Core/Src/MyCallbackFunc.c
@@ -0,0 +1,38 @@
+#include "MyCallbackFunc.h"
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)//外部中断回调处理函数
+{
+	if(GPIO_Pin==GPIO_PIN_7)
+	{
+	
+		 Add_FR();	
+		//press_FR();
+		HAL_UART_Transmit(&huart1,(uint8_t *)"touch",5,100);
+		
+		
+	}
+	
+	
+}
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)//定时器中断回调函数
+{
+	if(htim==&htim2)
+	{
+		HAL_UART_Transmit(&huart1,(uint8_t *)"nihao",4,100);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
