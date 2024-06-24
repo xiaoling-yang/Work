5f6275e641437cb19b43bbe72012ccedd0016f16
xiaoling-yang <yxl1939748084@163.com>
2024-06-24 10:57:49 +0800

    2024.6.24 功能实现，目前暂时未发现Bug

---
+++ Car_Terminal_6_24/Core/Inc/MyCallbackFunc.h
@@ -0,0 +1,12 @@
+#ifndef  __MYCALLBACKFUNC_H
+#define  __MYCALLBACKFUNC_H
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "AS608.h"
+#include "adc.h"
+#include "syn6288.h"
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);//外部中断回调处理函数
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);//定时器中断回调函数
+#endif
